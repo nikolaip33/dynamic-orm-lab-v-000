6b0b3f40b0263911b77f9712ded8279b8b47cb6f
Nikolai Pawlowski <nikolaip@gmail.com>
2017-10-25 22:22:46 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -7,4 +7,9 @@ class InteractiveRecord
     self.to_s.downcase.pluralize
   end
 
+  def self.column_names
+    DB[:conn].results_as_hash = true
+
+    sql = "pragma table_info('#{table_name}')"
+
 end
