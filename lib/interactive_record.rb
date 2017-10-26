0bc14f07f024520756599b21f9f745a85e2ad815
Nikolai Pawlowski <nikolaip@gmail.com>
2017-10-26 01:35:15 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -34,7 +34,7 @@ class InteractiveRecord
 
   def values_for_insert
     self.class.column_names.map do |col_name|
-      '#{send(col_name)}'
+      "'#{send(col_name)}'"
     end.flatten.join(", ")
   end
 
